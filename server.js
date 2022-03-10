093dce7af836c84fe60ac17b58f444450cce3795
fsf4965 <fsf4965@autuni.ac.nz>
2022-03-10 14:33:56 +1300

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ require("./app/routes/proof_file.routes")(app);
 require("./app/routes/feedback_file.routes")(app);
 //require("./app/routes/dev_project.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
